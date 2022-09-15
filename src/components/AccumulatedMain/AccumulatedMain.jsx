ff210765dfeb6dbffe388537908b4e3cd0abb932
Andrea Eurrieta <deyeurrieta@gmail.com>
2022-09-15 08:47:32 -0300

    Eliminación del botón ver más notas

--- src/components/AccumulatedMain/AccumulatedMain.jsx
+++ src/components/AccumulatedMain/AccumulatedMain.jsx
@@ -43,11 +43,6 @@ export const AccumulatedMain = () => {
                     />))
                 }
             </section>
-            <section className='row'>
-                <div className='col-12 hlp-text-center hlp-margintop-40'>
-                    <button className='--btn --secondary'>MÁS NOTAS DE ACUMULADO GRILLA</button>
-                </div>
-            </section>
         </div>
     )
 }
